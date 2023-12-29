b450e71fddf35d39527ed5aabb0bec5f2be89405
TaeHoon <31303655+xoasml@users.noreply.github.com>
2023-12-29 12:04:50 +0900

    add :ts로 리액트 상태 관리

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,14 @@
+
+;
+src/App.tsx,8/3/835da56f32df714aaed6285a5b7fc3a7807851e4
+=
+src/index.tsx,5/a/5ab44fe33ca96299f51ad4778a73e425b92bf439
+?
+src/Counter.tsx,e/0/e0efaa29bda45bfaf94d6f6b9d9703fdfcd19486
+A
+src/Greetings.tsx,4/d/4dcaea70a7768bdd38bf326c140eaee19610093a
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+>
+src/MyForm.tsx,b/7/b74a19d32d5f60f9413741f250d3238490324784
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Greetings from "./Greetings";
+import Counter from "./Counter";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const onClick = (name: string) => {
+        console.log(name)
+    }
+    return (
+        <>
+            <Greetings name="리액트" onClick={onClick}/>
+            <Counter/>
+        </>
+    );
 }
 
 export default App;
